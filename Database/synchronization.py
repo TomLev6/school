7279ff8868217c8391db160742eb8088d10b0f80
TomLev6 <100039158+TomLev6@users.noreply.github.com>
2022-11-10 12:35:32 +0200

    Add files via upload

---
+++ Database/synchronization.py
@@ -0,0 +1,47 @@
+import multiprocessing
+import threading
+from data_to_file import DataFile
+
+
+class Sync:
+    def __init__(self, db, initializer):
+        super().__init__()
+        self.initializer = initializer
+        self.dict = db
+        self.set_ready = True
+        if self.initializer:  # threading
+            self.lock = threading.Lock()
+            self.semaphore = threading.Semaphore(10)
+        else:  # processing
+            self.lock = multiprocessing.Lock()
+            self.semaphore = multiprocessing.Semaphore(10)
+
+    def set_value(self, key, val):
+        self.lock.acquire()
+        if self.set_ready:
+            self.dict[key] = val
+        self.lock.release()
+
+    def get_value(self, key):
+        self.semaphore.release()
+        return self.dict.get(key)
+
+    def delete_value(self, key):
+        self.lock.acquire()
+        val = self.get_value(key)
+        self.set_value(key, None)
+        self.lock.release()
+        return val
+
+    def __str__(self):
+        for key in self.dict.keys():
+            return "key: " + key \
+                   + "\nvalue: " + self.get_value(key)
+
+
+def main():
+    db = DataFile
+    syndb = Sync(db, True)
+
+
+# mode = Sync("threading")
