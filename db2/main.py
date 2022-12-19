c712f9b7c870d1aef710a3497922f213a00772d3
TomLev6 <100039158+TomLev6@users.noreply.github.com>
2022-12-19 11:09:38 +0200

    Delete db2 directory

--- db2/main.py
+++
@@ -1,4 +0,0 @@
-import win32event
-m = win32event.CreateMutex(None, False, 'MyMutex')
-r = win32event.WaitForSingleObject(m, 100)
-win32event.ReleaseMutex(m)
--- db2/main2.py
+++
@@ -1,3 +0,0 @@
-import win32event
-m2 = win32event.OpenMutex(win32event.SYNCHRONIZE, False, "MyMutex")
-r2 = win32event.WaitForSingleObject(m2, -1)
