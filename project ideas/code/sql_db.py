305108f92de4aa71c7901ac2a5724c6a7c42c9ff
TomLev6 <100039158+TomLev6@users.noreply.github.com>
2023-02-15 00:08:46 +0200

    Update sql_db.py

--- project ideas/code/sql_db.py
+++ project ideas/code/sql_db.py
@@ -1,37 +1,91 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+import datetime
 
-Base = declarative_base()
+import pyodbc
 
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    age = Column(Integer)
 
-engine = create_engine('sqlite:///example.db')
-Base.metadata.create_all(engine)
+class Odbc:
+    def __init__(self):
+        pass
+        # Connect to the database
+        self.DRIVER_NAME = 'SQL SERVER'
+        self.SERVER_NAME = "DESKTOP-4K52IRC"
+        self.DATABASE_NAME = 'SQLTutorial'
+        self.cursor = None
+        self.conn = None
+        # uid=<username>;
+        # pwd=<password>;
+        self.conn_string = f"""DRIVER={{{self.DRIVER_NAME}}};
+        SERVER={self.SERVER_NAME};DATABASE={self.DATABASE_NAME};
+        Trust_Connection=yes;"""
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    def connect(self):
+        self.conn = pyodbc.connect(self.conn_string)
+        # Create a cursor
+        self.cursor = self.conn.cursor()
 
-# Insert a new user
-new_user = User(name='John Doe', age=30)
-session.add(new_user)
-session.commit()
+    def insert_new_user(self, ip: str, packets: int, time: str):
+        # Insert data into the table
+        self.cursor.execute("INSERT INTO Users (userIP, PacketsAmount, StartTime) VALUES (?, ?, ?)",
+                            (ip, packets, time))
+        # Commit changes
+        self.conn.commit()
+        print("Commit changes..")
 
-# Query for all users
-users = session.query(User).all()
-for user in users:
-    print(user.id, user.name, user.age)
+    def insert_to_blacklist(self, ip: str, mode: str, starttime: datetime):
+        self.cursor.execute("INSERT INTO BlackList (userIP, mode, StartDate) VALUES (?, ?, ?)",
+                            (ip, mode, starttime))
+        # Commit changes
+        self.conn.commit()
+        print("Commit changes..")
 
-# Update a user
-user_to_update = session.query(User).filter(User.name=='John Doe').first()
-user_to_update.age = 31
-session.commit()
+    def close_connection(self):
+        # Close connection
+        self.conn.close()
 
-# Delete a user
-user_to_delete = session.query(User).filter(User.name=='John Doe').first()
-session.delete(user_to_delete)
-session.commit()
+    def find_in_users(self, ip: str):
+        self.cursor.execute(f"SELECT * FROM Users WHERE userIp ='{ip}';")
+        result = self.cursor.fetchall()
+        if len(result) == 0:
+            return True
+        else:
+            print("user already in users!")
+            return False
+
+    def find_in_blacklist(self, ip: str):
+        self.cursor.execute(f"SELECT * FROM BlackList WHERE userIp ='{ip}';")
+        result = self.cursor.fetchall()
+        if len(result) == 1:
+            return True
+        return False
+
+    def update_user_data(self, ip: str, packets: int):
+        self.cursor.execute("UPDATE Users SET userIP = ?, PacketsAmount = ?",
+                            (ip, packets))
+        self.conn.commit()
+        print("Commit changes..")
+
+    def get_user_packets(self, ip: str):
+        self.cursor.execute(f"SELECT * FROM Users WHERE userIp ='{ip}';")
+        res = str(self.cursor.fetchall())
+        if len(res) != 0:
+            return int(str(self.cursor.fetchall()).split(",")[1])
+
+    def get_user_time(self, ip: str):
+        self.cursor.execute(f"SELECT * FROM Users WHERE userIp ='{ip}';")
+        res = str(self.cursor.fetchall()).split(",")[2]
+        return res.split("'")[1]
+
+    def delete_user(self, ip: str, db: str):
+        self.cursor.execute(f"DELETE From {db} WHERE userIP ='{ip}';")
+        # Commit changes
+        self.conn.commit()
+        print("Commit changes..")
+
+
+o = Odbc()
+o.connect()
+# o.insert_new_user("100.111.111.111", 55, "2.3333")
+# o.update_user_data("100.111.111.111", 90)
+# print(o.get_user_time("100.111.111.111"))
+# o.find_in_users("100.111.111.111", 55, "2.3333")
+# o.delete_user("100.111.111.111", "Users")
