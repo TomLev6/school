94220c824b2b81d89a769a34fcb74b13d73c5e5f
TomLev6 <100039158+TomLev6@users.noreply.github.com>
2023-02-15 00:09:14 +0200

    Update defence.py

--- project ideas/code/defence.py
+++ project ideas/code/defence.py
@@ -4,49 +4,49 @@ from black_client import Black_client
 from scapy.all import *
 from scapy.packet import Packet
 from datetime import datetime
-import tkinter_gui
+from sql_db import Odbc
 
 packet_count = 0
 max_packets = 128  # (200)
 start_time = time.time()
-users_list = Clients()
-black_list = Black_client()
+# users_list = Clients()
+# black_list = Black_client()
+db = Odbc()
+db.connect()
 
 
 def handle_packets(packet: Packet):
     global packet_count, start_time
     if "IP" in str(packet.layers()):
         ip = packet[IP].src
-        if not users_list.search(ip):
-            users_list.set_packet_count(ip, 1)
-            users_list.set_time(ip, time.time())
+        if not db.find_in_users(ip):
+            db.insert_new_user(ip, 1, str(time.time()))
         else:
-            users_list.set_packet_count(ip, users_list.get_packets_count(ip) + 1)
-            if users_list.get_packets_count(ip) > 128:
+            db.update_user_data(ip, db.get_user_packets(ip) + 1)
+            if db.get_user_packets(ip) > 128:
                 # Check the time elapsed
-                elapsed_time = time.time() - users_list.get_time(ip)
+                elapsed_time = time.time() - db.get_user_time(ip)
                 # print(f"if {users_list.get_packets_count(ip)} > {max_packets} and {elapsed_time} < 2")
-                if users_list.get_packets_count(ip) > max_packets and elapsed_time < 3:  # 1
+                if db.get_user_packets(ip) > max_packets and elapsed_time < 3:  # 1
                     print("******************************  Attack detected!  ******************************")
                     now = datetime.now()
-                    black_list.set_mode(ip, True)
-                    black_list.set_time(ip, now.strftime("%H:%M:%S"))
+                    db.insert_to_blacklist(ip, "attacker", now)
             else:
                 # should route the request to the site
                 print("routing the request to the site...")
                 # print(users_list)
-                if len(black_list) > 0:
-                    print(black_list)
+                # if len(black_list) > 0:
+                #     print(black_list)
     else:
         print("Error!")
         pass
 
 
 def filters(pkt: Packet):
-    global black_list
+    global db
     if IP in pkt.layers():
         # if pkt[IP].src == "172.16.6.69":
-        if black_list.get_mode(pkt[IP].src):
+        if db.find_in_blacklist(pkt[IP].src):
             print(f"access denied!, blocked user({pkt[IP].src})!")
             return False
         return True
@@ -58,3 +58,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
